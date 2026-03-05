c0e507231702051245af2691661a15c0b7460d5c
sangeetha-karthikeyan <sangeetha.k2023@vitstudent.ac.in>
2026-03-05 17:21:53 +0530

    Add unit tests for EvenOdd class
    Added tests for the EvenOdd class to check even numbers.

--- test/java/example/Apptest.java
+++ test/java/example/Apptest.java
@@ -1,9 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test; { 
+import org.junit.jupiter.api.Test;
 
+public class EvenOddTest {
 
-}
+    private final EvenOdd evenOdd = new EvenOdd();
 
-
+    @Test
+    void testIsEvenWithEvenNumber() {
+        assertTrue(evenOdd.isEven(2), "2 should be even");
+        assertTrue(evenOdd.isEven(100), "100 should be even");
+    }
 
 
